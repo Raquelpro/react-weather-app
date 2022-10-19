309a52d490491c20af393c5ed476af77017e962c
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-19 12:05:42 -0500

    Styled project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div classname="container">
+      <Weather />
       <footer>
         This project was coded by Raquel Provvidente and is {""}
-      <a href="https://github.com/Raquelpro/react-weather-app" target="_blank" rel="noreferrer">
+      <a 
+      href="https://github.com/Raquelpro/react-weather-app" 
+      target="_blank" 
+      rel="noreferrer"
+      >
      open-sourced on GitHub
       </a>
       </footer>
+      </div>
       </div>
     );
 }
