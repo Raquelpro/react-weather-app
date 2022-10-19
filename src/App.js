fa7a1f00f39e04e15454372b8f0644c9e79515ad
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-19 10:46:38 -0500

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Raquel Provvidente and is {""}
+      <a href="https://github.com/Raquelpro/react-weather-app" target="_blank" rel="noreferrer">
+     open-sourced on GitHub
+      </a>
+      </footer>
+      </div>
     );
 }
 
-export default App;
