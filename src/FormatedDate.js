676d75ee4ebb8ad8bb82306a3277d3f94264a1a9
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-20 12:59:16 -0500

    Added date component to project

---
+++ src/FormatedDate.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+export default function FormatedDate(props) {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[props.date.getDay()];
+    let hours = props.date.getHours();
+    let minutes = props.date.getMinutes();
+ return (
+ <div>
+    {day} {hours}:{minutes}
+    </div>
+ );
+}
