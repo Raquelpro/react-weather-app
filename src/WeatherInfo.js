cff0e5eb8dc370d2b70e0c5a0b24f19252427740
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-21 13:34:36 -0500

    Updated weather info component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,11 +15,15 @@ export default function WeatherInfo(props) {
    </ul>
    <div className="row mt-3">
        <div className="col-6">
+        <div className="clearfix">
         <div className="float-left">     
    <WeatherIcon code={props.data.icon} />        
 </div>
+<div className="float-left">
 <span className="temperature">{Math.round(props.data.temperature)}</span>
 <span className="unit">°C</span>
+</div>
+       </div>
        </div>
        <div className="col-6">
 <ul>
